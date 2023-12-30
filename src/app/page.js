18a0e02837e829b1f053788e769e8dc791219078
waleedsaifullah <waleedsaifullah786@gmail.com>
2023-12-30 19:59:33 +0500

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 export default function Home() {
   return (
@@ -9,6 +10,33 @@ export default function Home() {
       <Header/>
       <Hero/>
       <HomeMenu/>
+
+      <section className="text-center my-16">
+        <SectionHeaders
+          subHeader={'Our Story'}
+          mainHeader={'About Us'}
+        />
+
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available</p>
+          <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available</p>
+          <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available</p>
+        </div>
+      </section>
+
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={'Don\'t hesitate'}
+          mainHeader={'Contact Us'}
+        />
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-500" href="tel:+923187664152">+92 318 7664152</a>
+        </div>
+      </section>
+
+      <footer className="border-t p-8 text-center text-gray-500 mt-16"> 
+        &copy; 2023 All rights reserved
+      </footer>
     </>
   )
 }
--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,10 +1,33 @@
+import Image from 'next/image'
 import React from 'react'
+import MenuItem from '../menu/MenuItem'
+import SectionHeaders from './SectionHeaders'
 
 export default function HomeMenu() {
     return (
-        <div>
-            <h3>Check out</h3>
-            <h2>Menu</h2>
-        </div>
+        <section>
+            <div className='absolute left-0 right-0 w-full justify-start'>
+                <div className='absolute left-0 -top-[70px] text-left -z-10'>
+                    <Image src={'/sallad1.png'} width={109} height={109} alt={'sallad'} />
+                </div>
+                <div className='absolute -top-[100px] right-0 -z-10'>
+                    <Image src={'/sallad2.png'} width={107} height={195} alt={'sallad'}/>
+                </div>
+            </div>
+            <div className='text-center mb-4'>
+                <SectionHeaders
+                    subHeader={'Check out'}
+                    mainHeader={'Menu'}
+                />
+            </div>
+            <div className='grid grid-cols-3 gap-4'>
+                <MenuItem/>
+                <MenuItem/>
+                <MenuItem/>
+                <MenuItem/>
+                <MenuItem/>
+                <MenuItem/>
+            </div>
+        </section>
     )
 }
